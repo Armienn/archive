fa5c02fb04c9f8e981f819b0d4c4747ce835678b
Armienn <nigathr@gmail.com>
2017-11-08 21:59:38 +0100

    Setting up structure

--- search/main.js
+++ search/main.js
@@ -4,15 +4,28 @@ var l = arf.l
 var Component = arf.Component
 
 class SearchSite extends Component {
+	constructor() {
+		super()
+		this.sections = {
+			header: new SectionHeader(),
+			navigation: new SectionNavigation(),
+			selection: new SectionSelection(),
+			content: new SectionContent(),
+			footer: new SectionFooter()
+		}
+	}
+
 	renderThis() {
 		return l("div", {},
-			l("header", "Hej du"),
-			l("nav", data.sectionNav),
-			l("section", "state.currentPage")
+			this.sections.header,
+			this.sections.navigation,
+			this.sections.selection,
+			this.sections.content,
+			this.sections.footer
 		)
 	}
 
-	static styleThis(){
+	static styleThis() {
 		return {
 			header: {
 				fontSize: "2rem",
@@ -31,90 +44,49 @@ class SearchSite extends Component {
 	}
 }
 
-class SectionNav extends Component {
+class SectionHeader extends Component {
 	renderThis() {
-		if(!this.components){
-			this.components = {
-				buttonThreads: new ButtonNav("Hjem", data.sectionThreads),
-				buttonImages: new ButtonNav("Billeder", data.sectionImages),
-				buttonCode: new ButtonNav("Sjov", data.sectionCode)
-			}
-		}
-		return l("div",
-			l("ul",
-				this.components.buttonThreads,
-				this.components.buttonImages,
-				this.components.buttonCode),
-			l("a", {}, "Log ud")
+		return l("header",
+			l("h1", "header"),
+			l("h2", "subheader")
 		)
 	}
-
-	renderHasChanged() {
-		return false
-	}
-
-	static styleThis() {
-		return {
-			"div": {
-				padding: "0.5rem",
-				display: "flex",
-				fontWeight: "bold",
-				fontSize: "1.2rem",
-			},
-			"ul": {
-				display: "flex",
-				flexGrow: 1
-			},
-			"a": {
-				flexGrow: 0,
-				cursor: "pointer"
-			}
-		}
-	}
 }
 
-class ButtonNav extends Component {
-	constructor(text, destination) {
-		super()
-		this.text = text
-		this.destination = destination
-	}
-
+class SectionNavigation extends Component {
 	renderThis() {
-		return l(state.currentPage == this.destination ? "li.selected" : "li", {
-			onclick: () => { this.selectPage(this.destination) }
-		}, this.text)
-	}
-
-	static styleThis() {
-		return {
-			"li": {
-				height: "2rem",
-				lineHeight: "2rem",
-				textAlign: "center",
-				width: "10rem",
-				cursor: "pointer"
-			},
-			".selected": {
-				background: "lightgrey"
-			}
-		}
-	}
-
-	selectPage(page) {
-		state.currentPage = page
-		site.update()
+		return l("nav",
+			l("ul",
+				l("li", "nav item 1"),
+				l("li", "nav item 2")
+			)
+		)
 	}
 }
 
-var data = {
-	sectionNav: new SectionNav()
+class SectionSelection extends Component {
+	renderThis() {
+		return l("section",
+			l("p", "selection info")
+		)
+	}
 }
 
-var state = {
-	currentPage: data.sectionThreads
+class SectionContent extends Component {
+	renderThis() {
+		return l("section",
+			l("div", "there'll be a list here")
+		)
+	}
 }
 
+class SectionFooter extends Component {
+	renderThis() {
+		return l("footer",
+			l("p", "footer")
+		)
+	}
+}
 
 window.onload = function () {
 	var site = new SearchSite()
