96bddd0b7a837ee833c67636b612ca36588feafc
Kristjan <nigathr@gmail.com>
2020-09-21 09:34:40 +0200

    Split default stuff into separate files

---
+++ kortspil/default-parts.js
@@ -0,0 +1,80 @@
+const defaultParts = {
+	immediateEffects: `[
+{
+	text: "Giv 1 skade til et væsen eller en spiller.",
+	cost: 1,
+},
+{
+	text: "Giv 2 skade til et væsen eller en spiller.",
+	cost: 2,
+},
+{
+	text: "Giv 3 skade til et væsen eller en spiller.",
+	cost: 3,
+},
+{
+	text: "Giv 4 skade til et væsen eller en spiller.",
+	cost: 4,
+},
+{
+	text: "Giv 5 skade til et væsen eller en spiller.",
+	cost: 5,
+},
+{
+	text: "Giv 10 skade til et væsen.",
+	cost: 5,
+},
+{
+	text: "Få 1 energi",
+	cost: 1,
+},
+]`,
+	permanentEffects: `[
+{
+	text: "+1 Forsvar",
+	helpText: "Når noget med Forsvar tager skade, trækkes mængden af forsvar fra skaden.",
+	cost: 1,
+},
+{
+	text: "+1 Angreb",
+	helpText: "Når noget med Angreb giver skade, lægges mængden af angreb til skaden.",
+	cost: 1,
+},
+{
+	text: "-1 Forsvar",
+	helpText: "Når noget med Forsvar tager skade, trækkes mængden af forsvar fra skaden.",
+	cost: -1,
+},
+{
+	text: "-1 Angreb",
+	helpText: "Når noget med Angreb giver skade, lægges mængden af angreb til skaden.",
+	cost: -1,
+},
+{
+	text: "Lifelink",
+	helpText: "Når noget med Lifelink giver skade, giver det lige så meget liv til sin side.",
+	cost: 1,
+},
+{
+	text: "Haste",
+	helpText: "Når noget med Lifelink giver skade, giver det lige så meget liv til sin side.",
+	cost: 1,
+},
+{
+	text: "Deathtouch",
+	helpText: "Når noget med Lifelink giver skade, giver det lige så meget liv til sin side.",
+	cost: 1,
+},
+{
+	text: "Flying",
+	helpText: "Når noget med Lifelink giver skade, giver det lige så meget liv til sin side.",
+	cost: 1,
+},
+{
+	text: "Reach",
+	helpText: "Når noget med Lifelink giver skade, giver det lige så meget liv til sin side.",
+	cost: 2,
+},
+]`,
+}
+
