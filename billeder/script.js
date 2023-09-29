5c218b3e3598ee73354e7685995146189e53b0bf
Armienn <nigathr@gmail.com>
2023-09-29 16:21:05 +0200

    Add Billeder project

---
+++ billeder/script.js
@@ -0,0 +1,57 @@
+"use strict"
+
+const images = []
+let grid
+
+window.onload = () => {
+	grid = document.getElementById("grid")
+	createInput()
+	showImages()
+}
+
+function createInput() {
+	const input = newElement("input")
+	input.type = "file"
+	input.webkitdirectory = true
+	input.addEventListener("change", loadFiles)
+	document.body.appendChild(input)
+}
+
+function loadFiles(event) {
+	images.splice()
+	grid.innerHTML = ""
+	const files = event.target.files
+	for (let file of files) {
+		if (file.type.startsWith("image")) {
+			const url = URL.createObjectURL(file)
+			images.push(url)
+		}
+	}
+	showImages()
+}
+
+function showImages() {
+	let index = 0
+	while (index < images.length) {
+		const image = newElement("img")
+		image.src = images[index]
+		grid.appendChild(image)
+
+		index = index + 1
+	}
+}
+
+function newElement(definition) {
+	const parts = definition.split(/([.#])/)
+	const tag = parts.shift()
+	const element = document.createElement(tag)
+	const classes = []
+	for (let i = 0; i < parts.length; i += 2) {
+		if (parts[i] == "#")
+			element.id = parts[i + 1]
+		else if (parts[i] == ".")
+			classes.push(parts[i + 1])
+	}
+	element.className = classes.join(", ")
+	return element
+}
